665e61fc4a0c7d1cd6c6e42b11b6506d4343600a
Anna Boettcher <anna.boettcher@codefrog-it.de>
2020-01-27 11:00:07 +0100

    Updates

---
+++ precache-manifest.0da9c1c9f36a4ae814d168f6c5054405.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88cc224e84514aedacae0ee6447925e3",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "f580c57c7763d017101d",
+    "url": "/hackernews/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6a1f8cbda18c9cf42d62",
+    "url": "/hackernews/static/js/2.ff4b4a9a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hackernews/static/js/2.ff4b4a9a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f580c57c7763d017101d",
+    "url": "/hackernews/static/js/main.62b552dc.chunk.js"
+  },
+  {
+    "revision": "0e2961c2df66a7a1db77",
+    "url": "/hackernews/static/js/runtime-main.376ba91b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hackernews/static/media/logo.5d5d9eef.svg"
+  }
+]);
